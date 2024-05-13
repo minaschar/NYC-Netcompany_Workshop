f865208fd3c00ac9c1385b452330eacf16e1be3a
MINAS\User <ics20072@uom.edu.gr>
2024-05-14 01:54:11 +0300

    Remove files to be implemented

--- back - end/ScoreApp/src/main/java/org/nyc/Repositories/MatchRepository.java
+++
@@ -1,7 +0,0 @@
-package org.nyc.Repositories;
-
-import org.nyc.Entities.MatchEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatchRepository extends JpaRepository<MatchEntity, Long> {
-}
